6a5c3a8fb136f8eb45c07e12d7628b7908d18bee
mobilitips <mobilitips@hotmail.com>
2015-02-13 12:45:54 +0100

    [AMS] Initial commit

---
+++ Motip.Web.AzureMobileService/Motip.Web.AzureMobileService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Motip.Web.AzureMobileService.WebApiApplication" Language="C#" %>
---
+++ Motip.Web.AzureMobileService/Motip.Web.AzureMobileService/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+namespace Motip.Web.AzureMobileService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register();
+        }
+    }
+}
